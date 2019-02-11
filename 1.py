8f3edd0371db1ca2a587ace066bae5e03bfa052e
3aBZ <47334885+3aBZ@users.noreply.github.com>
2019-02-11 20:26:29 +1300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,20 @@
-# -*- coding: utf-8 -*-
-list1=[]
+listS=[]
 while True:
-    couse=input()
-    if couse=='0':
-        break
-    list1.append(couse)
-    
-for i in range(len(list1)):
-    print(str(i+1)+'.'+list1[i])
+  subject=input("What subjects do you have:(Enter 0 to finish)\n")
+  if subject == "0":
+    break
+  listS.append(subject)
+
+for z in range(len(listS)):
+  print(str(z+1)+"."+listS[z])
+
+
+AskS=input("What's your favourite subject\n")
+
+AskC=input("What's your most challenging subject\n")
+
+
+ze = listS[int(a)-1]
+
+print("Your favourite subject is", ze)
+print("Your most challenging subject", ze)
